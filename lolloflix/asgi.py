12e05acec3ac12f3fe3c90b0f9113411e9b5a910
LorenzoMogicatoITS <lorenzo.mogicato@gmail.com>
2023-05-01 12:44:18 +0200

    change project name

--- lolloflix/asgi.py
+++ lolloflix/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for altacucina_test project.
+ASGI config for lolloflix project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "altacucina_test.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lolloflix_test.settings")
 
 application = get_asgi_application()
--- lolloflix/wsgi.py
+++ lolloflix/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for altacucina_test project.
+WSGI config for lolloflix project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "altacucina_test.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lolloflix.settings")
 
 application = get_wsgi_application()
