a9e5e009e467be02579d6f3e3729fe499668bd8b
LorenzoMogicatoITS <lorenzo.mogicato@gmail.com>
2023-05-03 17:17:40 +0200

    add tests

--- films/api/urls.py
+++ films/api/urls.py
@@ -3,8 +3,8 @@ from films.api.views import UserViewSet, FilmViewSet, ReviewCreateAPIView
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
-router.register(r"user", UserViewSet, basename="cazzoequa")
-router.register(r"film", FilmViewSet, basename="film")
+router.register("user", UserViewSet, basename="user")
+router.register("film", FilmViewSet, basename="film")
 
 urlpatterns = [
     path("film/<int:film_pk>/review/", ReviewCreateAPIView.as_view(), name="film-review"),
--- films/tests.py
+++ films/tests.py
@@ -1,3 +1,49 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from films.models import Film
+
+
+class RegistrationTestCase(APITestCase):
+    def test_registration(self):
+        data = {
+            "username": "lmogicato",
+            "email": "lorenzo.mogicato@gmail.com",
+            "password": "godohoPyCharmPROgratis#0362",
+            "re_password": "godohoPyCharmPROgratis#0362",
+        }
+        response = self.client.post("/auth/users/", data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+
+class ReviewTestCase(APITestCase):
+    film_list_url = reverse("film-list")
+
+    def setUp(self):
+        self.user = User.objects.create_user(username="lollomogicato0362", password="t1zi4noF3RR0@xyz")
+        self.token = Token.objects.create(user=self.user)
+        self.api_authentication()
+
+    def api_authentication(self):
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + self.token.key)
+
+    def test_seen_flag(self):
+        Film.objects.create(title="The Imitation Game", release_year=2014, platform="Netflix")
+
+        response = self.client.get(self.film_list_url, kwargs={"pk": 1})
+        self.assertEqual(False, response.json()[0]["seen"])
+
+        self.client.post(
+            reverse("film-review", kwargs={"film_pk": 1}),
+            {"vote": 9, "comment": "bu bu milano"}
+        )
+        response = self.client.get(self.film_list_url, kwargs={"pk": 1})
+        self.assertEqual(True, response.json()[0]["seen"])
+
+    def test_user_detail_retrieve(self):
+        response = self.client.get(reverse("user-detail", kwargs={"pk": 1}))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["username"], "lollomogicato0362")
